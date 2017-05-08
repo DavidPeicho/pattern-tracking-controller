22f369ebe71fd1596efde9b24f373a83ef7c3da1
AurelienLeandri <aurelien.leandri@gmail.com>
2017-05-08 14:59:43 +0200

    Reify hessian functions for easier debug and consistency

--- src/surf/surf.cpp
+++ src/surf/surf.cpp
@@ -24,6 +24,7 @@ namespace ptc {
       Utils::drawFeaturePoints(_img, _interestPoints);
     }
 
+
     void Surf::computeOrientations() {
       for (auto &e : _interestPoints) {
         float gauss = 0.f;
