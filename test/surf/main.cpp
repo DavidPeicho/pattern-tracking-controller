e040131b6bbf7b15a54e1570a43d83d117cae391
AurelienLeandri <aurelien.leandri@gmail.com>
2017-05-19 18:36:43 +0200

    Replace Flann with bruteforce matcher for testing

--- test/surf/main.cpp
+++ test/surf/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <ptc.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <surf/surf.hpp>
 #include <opencv2/imgcodecs/imgcodecs_c.h>
 #include <unistd.h>
@@ -16,11 +15,11 @@ int main() {
   //const char* testImgPath = "assets/cat.png";
   //const char* testImgPath = "assets/text.png";
   //const char* testImgPath = "assets/test.png";
-  const char* testImgPath = "assets/scene.png";
+  const char* testImgPath = "assets/input_photo.jpg";
   const char* arrowUpImgPath = "assets/arrow_up.jpg";
 
-  testFrame = cv::imread(testImgPath, CV_LOAD_IMAGE_COLOR);
-  arrowUpFrame = cv::imread(arrowUpImgPath, CV_LOAD_IMAGE_COLOR);
+  testFrame = cv::imread(testImgPath, CV_LOAD_IMAGE_GRAYSCALE);
+  arrowUpFrame = cv::imread(arrowUpImgPath, CV_LOAD_IMAGE_GRAYSCALE);
 
   if(!testFrame.data) {
     std::cerr <<  "Could not open or find the test image" << std::endl ;
@@ -31,10 +30,7 @@ int main() {
     return -1;
   }
 
-  // Initial image
-  cv::imwrite("test_input.png", testFrame);
-
-  int minHessian(400);
+  int minHessian(5000);
 
   // Keypoints
   cv::Ptr<cv::xfeatures2d::SurfFeatureDetector> detector = cv::xfeatures2d::SurfFeatureDetector::create(minHessian);
@@ -48,6 +44,15 @@ int main() {
   extractor->compute(arrowUpFrame, keypointsArrowUp, descriptorArrowUp);
   extractor->compute(testFrame, keypointsTest, descriptorTest);
 
+  cv::BFMatcher matcher;
+  std::vector<cv::DMatch> matches;
+  matcher.match(descriptorArrowUp, descriptorTest, matches);
+  cv::Mat img_matches;
+  drawMatches(arrowUpFrame, keypointsArrowUp, testFrame, keypointsTest,
+              matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+              std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+  /*
   // Matching using FLANN
   cv::FlannBasedMatcher matcher;
   std::vector<cv::DMatch> matches;
@@ -58,8 +63,8 @@ int main() {
   double dist = 0;
   for( int i = 0; i < descriptorArrowUp.rows; i++ ) {
     dist = matches[i].distance; // distance between the two descriptors that match
-    if( dist < min_dist ) min_dist = dist;
-    if( dist > max_dist ) max_dist = dist;
+    if(dist < min_dist) min_dist = dist;
+    if(dist > max_dist) max_dist = dist;
   }
 
   // Keep only the matches within 3 * min_dist
@@ -76,14 +81,14 @@ int main() {
   std::vector<cv::Point2f> obj;
   std::vector<cv::Point2f> scene;
 
+  // Get the keypoints from the good matches
   for(unsigned int i = 0; i < good_matches.size(); i++ ) {
-    // Get the keypoints from the good matches
     obj.push_back(keypointsArrowUp[good_matches[i].queryIdx].pt);
     scene.push_back(keypointsTest[good_matches[i].trainIdx].pt);
   }
 
   cv::Mat H = findHomography(obj, scene, CV_RANSAC);
-  //-- Get the corners from the image_1 ( the object to be "detected" )
+  // Get the corners from the image_1 ( the object to be "detected" )
   std::vector<cv::Point2f> obj_corners(4);
   obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint(arrowUpFrame.cols, 0);
   obj_corners[2] = cvPoint(arrowUpFrame.cols, arrowUpFrame.rows); obj_corners[3] = cvPoint(0, arrowUpFrame.rows);
@@ -100,14 +105,12 @@ int main() {
        scene_corners[3] + cv::Point2f(arrowUpFrame.cols, 0), cv::Scalar( 0, 255, 0), 4);
   line(img_matches, scene_corners[3] + cv::Point2f(arrowUpFrame.cols, 0),
        scene_corners[0] + cv::Point2f(arrowUpFrame.cols, 0), cv::Scalar( 0, 255, 0), 4);
+   */
 
   //-- Show detected matches
   if (img_matches.data == NULL)
     std::cerr << "Data of img_matches is null" << std::endl;
-  cv::namedWindow("Good Matches & Object detection", cv::WINDOW_AUTOSIZE); // Create a window for display.
-  imshow("Good Matches & Object detection", img_matches);
   cv::imwrite("test_output_cv.png", img_matches);
-  cv::waitKey(0);
 
 
   //cv::imwrite("test_output_own.png", testFrame);
@@ -117,10 +120,10 @@ int main() {
   ptc::surf::Surf surf(testFrame);
   surf.launch();
   cv::imwrite("test_output_own.png", testFrame);
+  */
 
   char temp[120];
   std::cout << "Results saved into " << (getcwd(temp, 120) ? std::string( temp ) : std::string("")) << std::endl;
-  */
 
   tracker->free();
 
